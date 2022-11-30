2c85ab1ba91811b579ee85172c4da452907d40e2
Keamogetswe <kmoseki021@student.wethinkcode.co.za>
2022-11-30 10:43:02 +0200

    removed ' for &apos;

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,6 @@ export default function Home() {
         height: '100vh',
         width: '100%',
         justifyContent: 'center',
-        // alignItems: 'center',
         backgroundColor: 'lightblue',
         textAlign: 'center',
     }
@@ -37,7 +36,8 @@ export default function Home() {
     }
 
     const TableDataStyle = {
-        maxWidth: '1vw'
+        maxWidth: '3vw',
+        padding: '2em'
     }
 
     const TableStyle = {
@@ -54,21 +54,20 @@ export default function Home() {
                     <img src='/self.jpg' width={200} height={300} style={ImageStyle}/>
                 </p>
                 <h2>ABOUT</h2>
-                <p style={ParagraphStyle}>I am a software developer passionate about problem-solving, and back-end development with a focus on development using the Java language. I am currently advancing my knowledge in programming at WeThinkCode_ using Python and Java as my programming languages.</p>
-                <p>
+                <p style={ParagraphStyle}>I am an aspiring software developer passionate about problem-solving, and back-end development with a focus on development using the Java language. I am currently advancing my knowledge in programming at WeThinkCode_ using Python and Java as my programming languages.</p>
+                <div>
                     <p>Check out my profile on</p>
                     <a href='https://www.linkedin.com/in/keamogetswemoseki/'>
                         <img src='/linkedin.svg' width={50} height={50}/>
-
                     </a>
-                </p>
+                </div>
 
             </FullpageSection>
             <FullpageSection style={WhySovTechStyle}>
                 <h1> Why Sovtech</h1>
-                <img src='/grad.svg' width={200} height={300}/>
-                <img src='/dev.svg' width={200} height={300}/>
-                <p style={ParagraphStyle}>SovTech is the leading custom development company that designs and builds custom-made web, mobile and cloud software solutions and that is a passion of mine. I believe in developing good quality web and mobile apps. </p>
+                <img src='/grad.svg' width={200} height={200}/>
+                <img src='/dev.svg' width={200} height={200}/>
+                <p style={ParagraphStyle}>SovTech is the leading custom development company that designs and builds custom-made web, mobile and cloud software solutions and that is a passion of mine. I believe in developing good quality web and mobile apps. I am eager to learn, passionate about this industry and I believe that the skills and qualities I have aquired are a match for programme. I would love to be a part of a leading company in South Africa that designs and builds custom software that not only grows world-class businesses but has an impact in the 4IR. </p>
                 <p>The company values of <em>#Smile</em>, <em>#Tech</em> and <em>#Results</em> resonate with me because I believe in working with people I like not only does it make the job fun but easier as well. Technology is our future. I believe in self-development so I plan on working hard to grow and bear fruits of my hard work into your company</p>
                 <img src='/build.svg' width={200} height={200}/>
 
@@ -76,55 +75,55 @@ export default function Home() {
             <FullpageSection style={PersonalProjectStyle}>
                 <h1> Personal Projects</h1>
                 <table style={TableStyle}>
+                    <tbody>
                     <tr>
                         <th>
                             <p>Personal Projects</p>
                         </th>
                         <th>
-                            <p>Languages I've worked With</p>
+                            <p>Languages I&apos;ve worked With</p>
                         </th>
                     </tr>
                     <tr>
                         <td style={TableDataStyle}>
-                            Project stuff
-                            <p>THese are my projects blah blah blah</p>
-                            <a href="https://www.google.com">
+                            <p>To better and strengthen my skills I have worked on a few of my personal projects and can be accessed below:</p>
+                            <a href="https://github.com/Keamogetswee">
                                 <img src='/github.png' width={50} height={50} style={ImageStyle}/>
 
                             </a>
 
                         </td>
-                        <td>
-                            Language thing
-                            <p>THese are my languages blah blah blah</p>
-                            <a href="https://www.google.com">
+                        <td style={TableDataStyle}>
+                            <p>The following are languages I am familiar with and have used in my programming journey. I am also advancing my skills in ReactJs and Web development using Html, CSS, and Javascript to better understand the different styles and strategies used to build quality web applications.</p>
+                            <a href="https://docs.oracle.com/javase/7/docs/api/">
                                 <img src='/java.svg' width={50} height={50} style={ImageStyle}/>
 
-                            </a><a href="https://www.google.com">
+                            </a><a href="https://docs.python.org/3/">
                                 <img src='/python.svg' width={50} height={50} style={ImageStyle}/>
 
-                            </a><a href="https://www.google.com">
+                            </a><a href="https://dev.mysql.com/doc/">
                                 <img src='/mysql.svg' width={50} height={50} style={ImageStyle}/>
 
-                            </a><a href="https://www.google.com">
+                            </a><a href="https://docs.flutter.dev/">
                                 <img src='/flutter.svg' width={50} height={50} style={ImageStyle}/>
 
-                            </a><a href="https://www.google.com">
+                            </a><a href="https://reactjs.org/docs/getting-started.html">
                                 <img src='/react.svg' width={50} height={50} style={ImageStyle}/>
 
-                            </a><a href="https://www.google.com">
+                            </a><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
                                 <img src='/js.svg' width={50} height={50} style={ImageStyle}/>
 
-                            </a><a href="https://www.google.com">
+                            </a><a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
                                 <img src='/html.svg' width={50} height={50} style={ImageStyle}/>
 
-                            </a><a href="https://www.google.com">
+                            </a><a href="https://www.w3schools.com/cssref/index.php">
                                 <img src='/css.svg' width={50} height={50} style={ImageStyle}/>
 
                             </a>
                         </td>
 
                     </tr>
+                    </tbody>
                 </table>
             </FullpageSection>
         </FullPageSections>
